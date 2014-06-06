2188630369c1ba1643cb2556d0ae9bdd77a7fc7c
Konrad Borowski <x.fix@o2.pl>
2014-06-06 19:30:22 +0200

    Accidentally reversed grammar.

--- lib/Acme/DSON/Grammar.pm
+++ lib/Acme/DSON/Grammar.pm
@@ -3,10 +3,10 @@ use JSON::Tiny::Grammar;
 grammar Acme::DSON::Grammar is JSON::Tiny::Grammar;
 
 rule object    { 'such' ~ 'wow' <pairlist> }
-rule pairlist  { <pair> * % [next | and | also] }
+rule pairlist  { <pair> * % [next | <[,.!?]>] }
 rule pair      { <string> is <value> }
 rule array     { 'so' ~ 'many' <arraylist> }
-rule arraylist { <value> * % [next | <[,.!?]>] }
+rule arraylist { <value> * % [next | and | also ] }
 
 token value:sym<number> {
     :i
